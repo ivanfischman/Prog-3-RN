5c1351e41b50dbf63bcb39a4dbebd16e5fce3a85
jmendonca19 <jmendonca@udesa.edu.ar>
2022-11-10 09:43:56 -0300

    orden de posteos

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
     }
     
     componentDidMount(){
-        db.collection('posts').onSnapshot(
+        db.collection('posts').orderBy("createdAt", "desc").onSnapshot(
             docs => {
                 let posts = [];
                 docs.forEach( oneDoc => {
